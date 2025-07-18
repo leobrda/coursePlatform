c0e68ca228dc731057061a4be36af47e1f61d3f8
leobrda <leonardo-breda1@hotmail.com>
2025-07-18 14:34:53 -0300

    feat(notifications): add signals to auto-generate notifications on new questions and answers

--- cursos/signals.py
+++ cursos/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Resposta, Notificacao
+from .models import Resposta, Pergunta, Notificacao
 
 
 @receiver(post_save, sender=Resposta)
@@ -13,4 +13,16 @@ def criar_notificacao_de_resposta(sender, instance, created, **kwargs):
             Notificacao.objects.create(
                 destinatario=autor_pergunta,
                 resposta=instance
+            )
+
+
+@receiver(post_save, sender=Pergunta)
+def criar_notificacao_de_pergunta(sender, instance, created, **kwargs):
+    if created:
+        instrutor = instance.aula.curso.organizacao.dono
+
+        if instance.usuario != instrutor:
+            Notificacao.objects.create(
+                destinatario=instrutor,
+                pergunta=instance
             )
